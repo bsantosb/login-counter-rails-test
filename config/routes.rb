71762c17607bf4f45674bc37313e5a0c0471d2fa
Beatriz Santos <bsantosb@snu.ac.kr>
2015-04-20 20:17:16 +0900

    testing Session- week8

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'logout' => 'users#logout'
   post 'clearData' => 'users#clear_data'
   
+  post 'userCount' => 'users#user_count'  
+
   # resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
